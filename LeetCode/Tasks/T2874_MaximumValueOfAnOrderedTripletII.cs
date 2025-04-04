b27c93176813dd852a0ca234df98a5368397f774
Tigran <davidyan.tigran.git@gmail.com>
2025-04-04 07:54:51 +0300

    Implement task 2874

---
+++ LeetCode/Tasks/T2874_MaximumValueOfAnOrderedTripletII.cs
@@ -0,0 +1,16 @@
+namespace LeetCode.Tasks;
+
+public class T2874_MaximumValueOfAnOrderedTripletII
+{
+    public long MaximumTripletValue(int[] nums) {
+        long maxTriplet = 0, iMax = 0, dMax = 0;
+        
+        foreach (var kNum in nums)
+        {
+            maxTriplet = Math.Max(maxTriplet, dMax * kNum);
+            dMax = Math.Max(dMax, iMax - kNum);
+            iMax = Math.Max(iMax, kNum);
+        }
+        return maxTriplet;
+    }
+}
