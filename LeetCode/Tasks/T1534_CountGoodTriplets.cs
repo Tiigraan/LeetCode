d52fc39af6a33b074ddb80093cfe707c7dd72ea0
Tigran <davidyan.tigran.git@gmail.com>
2025-04-14 23:01:31 +0300

    Implement task 1534

---
+++ LeetCode/Tasks/T1534_CountGoodTriplets.cs
@@ -0,0 +1,20 @@
+namespace LeetCode.Tasks;
+
+public class T1534_CountGoodTriplets
+{
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        var count = 0;
+        for (var i = 0; i < arr.Length; i++)
+        for (var j = i + 1; j < arr.Length; j++)
+        for (var k = j + 1; k < arr.Length; k++)
+        {
+            if (Math.Abs(arr[i] - arr[j]) <= a &&
+                Math.Abs(arr[j] - arr[k]) <= b &&
+                Math.Abs(arr[i] - arr[k]) <= c)
+                count++;
+        }
+
+        return count;
+    }
+}
