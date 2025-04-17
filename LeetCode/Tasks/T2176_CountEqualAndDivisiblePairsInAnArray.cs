d553591f850974327ca2369e27eb4ac2993449f9
Tigran <davidyan.tigran.git@gmail.com>
2025-04-17 23:33:04 +0300

    Implement task 2176

---
+++ LeetCode/Tasks/T2176_CountEqualAndDivisiblePairsInAnArray.cs
@@ -0,0 +1,14 @@
+namespace LeetCode.Tasks;
+
+public class T2176_CountEqualAndDivisiblePairsInAnArray
+{
+    public int CountPairs(int[] nums, int k) {
+        var count = 0;
+        for (var i = 0; i < nums.Length; i++)
+        for (var j = i + 1; j < nums.Length; j++)
+            if (nums[i] == nums[j] && (i * j) % k == 0)
+                count++;
+            
+        return count;
+    }
+}
